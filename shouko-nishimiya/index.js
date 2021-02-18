0e74852c8a4117ac6f5e78dbb207cc3a84fd1044
Faith1sGay <daniel.viola001@mymdc.net>
2021-02-18 12:33:46 -0500

    oops, i forgot to add the changes from this file

--- shouko-nishimiya/index.js
+++ shouko-nishimiya/index.js
@@ -1,8 +1,14 @@
 const discord = require('discord.js')
 const { token, topgg } = require('./config.json')
 const client = new discord.Client()
-const dbl = require('dblapi.js')
-const DBL = new dbl(topgg, client)
+const topGG = require('@top-gg/sdk')
+const api = new topGG.Api(topgg)
+setInterval(() => {
+  api.postStats({
+    serverCount: client.guilds.cache.size,
+    shardCount: client.ws.shards.size
+  })
+}, 2700000)
 require('./functions/func.js')(client)
 require('./functions/events.js')(client)
 client.login(token)
