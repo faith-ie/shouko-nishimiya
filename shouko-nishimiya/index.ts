55052dbb88a8de133610f92d893bf9b3d397d419
Faith1sGay <daniel.viola001@mymdc.net>
2021-02-25 21:00:53 -0500

    I'm like really tired rn and will finish this later but it works;tm:

--- shouko-nishimiya/index.ts
+++ shouko-nishimiya/index.ts
@@ -1,14 +1,7 @@
-import { Client } from 'discord.js'
-import { token, Topgg } from './config.json'
-import * as topgg from '@top-gg/sdk' // I have to use npm because berry is stinky and didn't publish to yarn
-const api = new topgg.Api(Topgg)
-const client = new Client()
-setInterval(() => {
-  api.postStats({
-    serverCount: client.guilds.cache.size,
-    shardCount: client.options.shardCount
-  })
-}, 1800000)
-require('./functions/events')(client)
-require('./functions/func.js')(client)
-client.login(token)
+import Eris = require('eris')
+import ini = require('ini')
+import fs = require('fs')
+const readConfig = ini.parse(fs.readFileSync('./config.ini', 'utf-8').toString());
+// @ts-ignore
+const client = new Eris(readConfig.TOKEN);
+client.connect()
