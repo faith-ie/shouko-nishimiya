d59d22455ddf55ca41b810996030ec253923ec81
Faith1sGay <daniel.viola001@mymdc.net>
2021-03-26 13:05:04 -0400

    oops i forgot i never updated this

--- shouko-nishimiya/cmds/general/stats.js
+++ shouko-nishimiya/cmds/general/stats.js
@@ -1,15 +1,16 @@
 const discord = require('discord.js')
 const humanize = require('humanize-duration')
+const pretty = require('pretty-bytes')
 const { botVersion, clientId, owners } = require('../../config.json')
 module.exports.run = async (client, message, args) => {
-  const usedMem = process.memoryUsage().rss / 1024 / 1024
+  const usedMem = process.memoryUsage().rss
   const getSeconds = humanize(client.uptime, { round: true }, false)
   const totalMem = Math.ceil(require('os').totalmem() * 100) / 100
   const e = new discord.MessageEmbed()
   e.setTitle(`${client.user.username} v${botVersion}`)
   e.addField('Author', 'FVSAEZI#2700')
   e.addField('Bot Id', `${clientId}`)
-  e.addField('Memory', '**' + usedMem + '**' + 'mb' + ' / ' + '**' + totalMem / 1e+6 + '**' + 'mb')
+  e.addField('Memory', pretty(usedMem) + ' / ' + pretty(totalMem))
   e.addField('Owner IDs', `${owners}`)
   e.addField('Uptime', getSeconds)
   e.addField('Discord.js Version', discord.version)
